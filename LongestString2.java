d58cdcc27295a3442eb7335e51e7f554e2e303d5
Sukumaporn2546 <muaunwongtum_s@silpakorn.edu>
2024-04-22 15:35:09 +0700

    Create LongestString2.java

---
+++ LongestString2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LongestString2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        String MaxText = "";
+        for(int i=0;i<5;i++){
+            String str = sc.nextLine();
+            int LongestString = str.length();
+            if(LongestString>max){
+                max = LongestString;
+                MaxText= str;
+            }
+        }
+        System.out.println(MaxText);
+    }
+}
